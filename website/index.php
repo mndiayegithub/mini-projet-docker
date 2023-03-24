f1fd663fc7f6e8dcd897fed7962e1852138d40bb
mndiayegithub <mndiayepro97@gmail.com>
2023-03-24 00:28:57 +0000

    index.php modified

--- website/index.php
+++ website/index.php
@@ -26,11 +26,11 @@
                 "header" => "Authorization: Basic " . base64_encode("$username:$password"),
               )));
 
-              $url = 'http://<api_ip_or_name:port>/pozos/api/v1.0/get_student_ages';
+              $url = 'http://student_list_api:5000>/pozos/api/v1.0/get_student_ages';
               $list = json_decode(file_get_contents($url, false, $context), true);
               echo "<p style='color:red;; font-size: 20px;'>This is the list of the student with age</p>";
               foreach($list["student_ages"] as $key => $value) {
-                  echo "- $key are $value years old <br>";
+                  echo "- $key is $value <br>";
               }
              }
             ?>
